ac11176600e22f2f4ad0940ca1e55cb75996c817
Rafael Simionato <rafaasimi@hotmail.com>
2025-02-19 14:55:03 -0300

    feat: add scripts e readme

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# windows-winget-packages
-⭐ Script responsável por automatizar a instalação de softwares após instalação/reinstalação do Windows.
+# Windows Winget Packages
+
+## Descrição
+
+Scripts criados para automatizar a instalação de softwares no Windows.
+
+## Instalação
+
+Primeiramente é necessário ter o `winget` instalado na máquina. Versões mais recentes do `Windows 10` e `Windows 11` já possuem esse recurso.
+
+[Microsoft | Instalar Winget](https://learn.microsoft.com/pt-br/windows/package-manager/winget/#install-winget)
+
+Para validar a instalação, abra o terminal e execute:
+
+```
+winget -v
+```
+
+## Como utilizar
+
+Em `scripts` existem dois modelos que faço uso em meu computador pessoal e para meu computador corporativo. Você pode baixar qualquer um deles e modificar conforme necessidade.
+
+Basta executar como administrador o `nome-script.bat` e seguir com as instalações.
+
+## Dicas
+
+É possível alterar os scripts e especificar as suas próprias aplicações para serem instaladas.
+
+Para descobrir o nome dos pacotes, existem duas maneiras:
+
+### Linha de comando
+
+Via linha de comando, basta pesquisar pelo nome da aplicação ou palavra chave.
+
+```
+winget search 'Google Chrome'
+```
+
+<img src='./.github/terminal-winget-search.png'>
+
+Utilizar apenas o nome do pacote, irá instalar a última versão do pacote. Caso queira especificar uma versão, utilize:
+
+```
+winget install --id Google.Chrome --version 91.0.4472.124 --silent
+```
+
+### Winget.run
+
+No site [winget.run](https://winget.run/) ou [winstall.app](https://winstall.app/) é possível buscar por diversos pacotes, basta clicar no pacote desejado que o comando será salvo na sua área de transferência.
+
+## Importar / Exportar pacotes
+
+Outra forma de automatizar as instalações, é exportar a lista atual de pacotes e apenas importa-la em outro computador ou nova instalação.
+
+```
+winget export
+```
+
+```
+winget import
+```
+
+## Exemplo de execução
+
+<img src="./.github/terminal-winget-instalacao.png">
---
+++ scripts/maquina_pessoal.bat
@@ -0,0 +1,74 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Verifica se o winget está instalado
+where winget >nul 2>nul
+if %errorlevel% neq 0 (
+    echo winget não encontrado.
+    set /p installWinget="Deseja instalar winget? (s/n): "
+    if /i "!installWinget!"=="s" (
+        powershell -Command "Start-Process msstore: -ArgumentList '/pdp?productid=9NBLGGH4NNS1' -NoNewWindow -Wait"
+        timeout /t 30
+    ) else (
+        echo winget não será instalado. Saindo do script.
+        exit /b
+    )
+)
+
+:: Definir a lista de pacotes para instalar
+set "packages=Google.Chrome Deezer.Deezer Notion.Notion WhatsApp.WhatsApp Valve.Steam EpicGames.EpicGamesLauncher OpenJS.NodeJS.LTS Git.Git Microsoft.VisualStudioCode JetBrains.IntelliJIDEA.Community Figma.Figma"
+
+:: Exibir pacotes que serão instalados
+echo Os seguintes pacotes serao instalados:
+for %%i in (%packages%) do (
+    echo - %%i
+)
+
+echo.
+
+:: Pedir confirmação do usuário
+set /p confirm="Deseja continuar com a instalacao desses pacotes? (s/n): "
+if /i "!confirm!" neq "s" (
+    echo Instalação cancelada pelo usuário.
+    exit /b
+)
+
+:: Instalar pacotes
+set "failed_packages="
+set "successful_packages="
+
+for %%i in (%packages%) do (
+    echo.
+    echo Instalando %%i...
+    winget install --id %%i --silent
+    if !errorlevel! neq 0 (
+        echo Erro ao instalar %%i
+        set "failed_packages=!failed_packages! %%i"
+    ) else (
+        echo Sucesso ao instalar %%i
+        set "successful_packages=!successful_packages! %%i"
+    )
+)
+
+:: Lista pacotes que falharam na instalação
+if defined failed_packages (
+    echo.
+    echo Os seguintes pacotes nao foram possiveis instalar: !failed_packages!
+    for %%i in (%failed_packages%) do (
+    echo - %%i
+    )
+)
+
+:: Lista pacotes que foram instalados com sucesso
+if defined successful_packages (
+    echo.
+    echo Os seguintes pacotes foram instalados com sucesso: !failed_packages!
+    for %%i in (%successful_packages%) do (
+    echo - %%i
+    )
+)
+
+echo.
+
+endlocal
+pause
